e7a54d25d8952f3fa75c87e18c4611c344330583
Nassime-L <nassime.lahbib@univ-rouen.fr>
2023-09-06 17:00:11 +0200

    màj fichier

---
+++ TP1/fonctions.py
@@ -0,0 +1,8 @@
+def puissance(a,b):
+	if not type(a) and type(b) is int:
+		print("ce ne sont pas des entiers")
+	z = a**b
+	return z
+
+		
+	
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,5 @@
-print(" Hello , World ! ")
+import fonctions as f 
+a=int(input(" un nombre ? : "))
+b=int(input(" un autre nombre ? : "))
+res = f.puissance(a,b)
+print(" le résultat est : ", res)
