ac68dc33db241903b3a9e7679efbaf2073baf9b8
Nassime-L <nassime.lahbib@univ-rouen.fr>
2023-09-28 15:43:18 +0200

    màj fichier

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,7 +3,7 @@ def puissance(a,b):
 	if not type(a) is int:
 		raise TypeError("Only integers are allowed")
 	if a == 0 & b < 0 :
-		raise TypeError("Impossible ! ")
+		raise ArithmeticError
 	
 	z = a**b
 	return z
