43fa6baa105b7b7c9114f98bd5204c42936ee5e8
Nassime-L <nassime.lahbib@univ-rouen.fr>
2023-09-28 15:36:36 +0200

    màj fichier

--- TP1/test.py
+++ TP1/test.py
@@ -7,5 +7,5 @@ def test_2():
 	assert f.puissance( 0, -2 ) == 3
 	assert f.puissance( -6 , 2 ) == 36
 def test_3():
-	if a = 0 and b < 0 :
+	if a == 0 and b < 0 :
 		raise TypeError("Impossible ! ") 
