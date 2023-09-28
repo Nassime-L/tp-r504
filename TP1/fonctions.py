3a66894a14757e64f30483acc05beab10ac919b2
Nassime-L <nassime.lahbib@univ-rouen.fr>
2023-09-28 16:00:21 +0200

    màj fichier

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,7 +3,8 @@ def puissance(a,b):
 	if not type(a and b) is int:
 		raise TypeError("Only integers are allowed")
 	
-		
+	if a == 0 and b<0 :
+		raise ArithmeticError("Impossible !") 	
 		
 	
 	z = a**b
--- TP1/test.py
+++ TP1/test.py
@@ -7,4 +7,5 @@ def test_2():
 	assert f.puissance( -2, 2 ) == 4
 	assert f.puissance( -6 , 2 ) == 36
 def test_3():
-	pass	
+	assert f.puissance( 0, -2 )
+	with pytest.raises(ArithmeticError): f.puissance( 0, -2 )
