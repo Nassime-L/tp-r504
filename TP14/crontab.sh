ee5ef48b36614aaf362ba1e21a2b8bf3dcd3d17d
Nassime_TP4 <Nassime-L@nassime.lahbib@univ-rouen.fr>
2024-03-20 09:34:24 +0100

    question 1.5

---
+++ TP14/crontab.sh
@@ -0,0 +1,2 @@
+crontab -u $USER crontab
+echo "Recherche de vulnérabilités seront effectués tout les jours à 2h30 !"
---
+++ TP14/script.sh
@@ -0,0 +1,5 @@
+#redirection des CVE vers un fichier texte
+debsecan --suite=bullseye --format detail > ~/tp-r504/TP14/CVE_$(date +\%Y\%m\%d).txt
+
+
+
